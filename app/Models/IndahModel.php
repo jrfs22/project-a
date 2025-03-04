526e5f536eb007cacda6bca4a24305877c5405df
jrfs22 <mr.jrfs22@gmail.com>
2025-03-04 15:52:04 +0700

    update

---
+++ app/Models/IndahModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class IndahModel extends Model
+{
+
+    protected $table = 'indah';
+
+    protected $fillable = [
+        'name'
+    ];
+
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ProductSeeder::class,
-            JosepSeeder::class
+            JosepSeeder::class,
+            IndahSeeder::class
         ]);
     }
 }
